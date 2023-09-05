24ec8a475fc8d26423f556803d6789e06adba3d4
Hemily Batista <hemilyoliveira02@gmail.com>
2023-09-05 11:27:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ function contando() {
     let passo = document.querySelector('input#pass')
     let res = document.querySelector('div#res')
 
-    if (cmc.value.length == 0 || end.value.length == 0 || passo.value.length == 0) { //'se o valor de input for igual a 0'
+    if (cmc.value.length == 0 || end.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossível contar!'
     } else {
         res.innerHTML = 'Contando: '
 
-        let i = Number(cmc.value) //torna os valores utilizaveis
+        let i = Number(cmc.value)
         let f = Number(end.value)
         let p = Number(passo.value)
 
-        if (p <= 0) { //caso o PASSO seja 0
+        if (p <= 0) {
             window.alert('Passo inválido, considerando PASSO 1')
-            p = 1 //PASSO começa a valer 1
+            p = 1
         }
-        if (i < f) { //contagem crescente
-        for (let c = i; c <= f; c += p) { //a contagem começa pelo INICIO, enquanto C for menor ou igual ao final, C recebe ele mesmo mais o PASSO.
+        if (i < f) {
+        for (let c = i; c <= f; c += p) {
             res.innerHTML += `${c} \u{1F449}`//emoji de maozinha
-        } //innerHTML vai ser o que ele já tem + C
+        }
         } else {
-            for (let c = i; c >= f; c += p) {//contagem regressiva
+            for (let c = i; c >= f; c += p) {
                 res.innerHTML += `${c} \u{1F449}`
             }
         }
